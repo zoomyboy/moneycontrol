71f559ae89e60774ce6bf71d25ee1c88feeb3313
Philipp Lang <pille@zoomyboy.de>
2018-06-29 00:39:27 +0200

    Add routes file

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{vue}', ['as' => 'home', 'uses' => 'HomeController@index'])
+    ->where('vue', '[0-9A-Za-z\/\-]*');
