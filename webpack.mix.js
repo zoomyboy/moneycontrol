ab2cd47ba6705015538ca68ca03c095c294e200e
Philipp Lang <pille@zoomyboy.de>
2018-06-29 00:39:09 +0200

    Add Webpack file

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,17 @@ let mix = require('laravel-mix');
  |
  */
 
+mix.webpackConfig({
+    module: {
+        loaders: [{
+            test: /\.styl$/,
+            exclude: /node_modules\/.*/,
+            loader: 'stylus-loader'
+        }]
+    }
+});
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .js('resources/assets/js/login.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .stylus('resources/assets/stylus/app.styl', 'public/css/vuetify.css');
